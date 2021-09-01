16289b936c7090ce64f3d9b6bfe5db8d1a036ba3
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-09-01 12:35:56 -0300

    Código refatorado. Agora é mais flexível, legível e de fácil reutilização.

--- app/src/main/java/com/lucascabral/composeui/MainActivity.kt
+++ app/src/main/java/com/lucascabral/composeui/MainActivity.kt
@@ -1,38 +1,55 @@
 package com.lucascabral.composeui
 
 import android.os.Bundle
+import android.widget.HorizontalScrollView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lucascabral.composeui.ui.theme.ComposeUiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeUiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Greeting(name = "Lucas Cabral")
             }
         }
     }
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeUiTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Green) {
+        Text(
+            text = "Hello $name!",
+            color = Color.Red,
+            modifier = Modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeUiTheme {
-        Greeting("Android")
+    MyApp {
+        Greeting(name = "Lucas Cabral")
     }
 }
