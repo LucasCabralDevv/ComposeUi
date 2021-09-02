0d877e38a5b0035505f280b6333d8d42b8a9fc95
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-09-02 17:51:01 -0300

    Icon de favorito adicionado ao TopAppBar.

--- app/src/main/java/com/lucascabral/composeui/MaterialComponentsActivity.kt
+++ app/src/main/java/com/lucascabral/composeui/MaterialComponentsActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,6 +32,10 @@ fun LayoutsTest() {
         topBar = {
             TopAppBar(title = {
                 Text(text = "Material Components")
+            }, actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
             })
         }
     ) { innerPadding ->
