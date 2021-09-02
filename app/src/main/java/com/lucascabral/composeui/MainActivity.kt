2373c415ae98a5c4cc79eecab696368e7d11d470
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-09-02 10:31:08 -0300

    Modificando o background color dos itens da lista de acordo com click.

--- app/src/main/java/com/lucascabral/composeui/MainActivity.kt
+++ app/src/main/java/com/lucascabral/composeui/MainActivity.kt
@@ -3,15 +3,16 @@ package com.lucascabral.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,11 +35,11 @@ fun Counter(
     count: Int, updateCount: (Int) -> Unit
 ) {
     Button(
-        onClick = { updateCount(count+1) },
+        onClick = { updateCount(count + 1) },
         colors = ButtonDefaults.buttonColors(
             backgroundColor = if (count > 5) Color.Green else Color.White
         )
-        ) {
+    ) {
         Text("I've been clicked $count times")
     }
 }
@@ -82,13 +83,16 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = Color.Yellow) {
-        Text(
-            text = name,
-            color = Color.Black,
-            modifier = Modifier.padding(24.dp)
-        )
-    }
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+
+    Text(
+        text = name,
+        modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected })
+    )
 }
 
 @Preview(showBackground = true)
