8960044661d01218d26994554d897566aa7706d5
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-09-03 17:29:52 -0300

    Adicionado um vertical scroll para o column.

--- app/src/main/java/com/lucascabral/composeui/ListsActivity.kt
+++ app/src/main/java/com/lucascabral/composeui/ListsActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lucascabral.composeui.ui.theme.ComposeUiTheme
 
@@ -24,9 +27,10 @@ class ListsActivity : ComponentActivity() {
 
 @Composable
 fun SimpleList() {
-    Column {
+    val scrollState = rememberScrollState()
+    Column(Modifier.verticalScroll(scrollState)) {
         repeat(100) {
-            Text("Item $it")
+            Text("Item #$it")
         }
     }
 }
