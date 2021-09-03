9d8d3012054deafcb7b11e318f8bf323d42d06a3
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-09-03 17:57:37 -0300

    Adicionado imagens com a lib coil compose.

--- app/src/main/java/com/lucascabral/composeui/ListsActivity.kt
+++ app/src/main/java/com/lucascabral/composeui/ListsActivity.kt
@@ -3,7 +3,8 @@ package com.lucascabral.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
@@ -12,8 +13,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.lucascabral.composeui.ui.theme.ComposeUiTheme
 
 class ListsActivity : ComponentActivity() {
@@ -42,8 +46,24 @@ fun LazyList() {
     val scrollState = rememberLazyListState()
     LazyColumn(state = scrollState) {
         items(100) {
-            Text("Item #$it")
+            ImageListItem(index = it)
         }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
