ee02130d19328508e0a6cd75f4be0a92c06dc228
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-09-09 18:34:29 -0300

    Utilizando layout modifier para configurar manualmente o posicionamente de um elemento.

--- app/src/main/java/com/lucascabral/composeui/CustomLayoutActivity.kt
+++ app/src/main/java/com/lucascabral/composeui/CustomLayoutActivity.kt
@@ -3,11 +3,15 @@ package com.lucascabral.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.lucascabral.composeui.ui.theme.ComposeUiTheme
 
 class CustomLayoutActivity : ComponentActivity() {
@@ -15,16 +19,33 @@ class CustomLayoutActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeUiTheme {
-
+                Text("Hello world!", Modifier.firstBaselineToTop(32.dp))
             }
         }
     }
 }
 
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview4() {
     ComposeUiTheme {
-
+        Text("Hello world!", Modifier.firstBaselineToTop(32.dp))
     }
 }
